a67672dc90624d2d47d693ef721a1b67a2219e13
marcor2742 <marcoruggiero.mail@gmail.com>
2024-01-08 18:45:29 +0100

    Update pipex.c and pipex.h

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: mruggier <mruggier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 15:07:15 by mruggier          #+#    #+#             */
-/*   Updated: 2024/01/05 18:43:01 by mruggier         ###   ########.fr       */
+/*   Updated: 2024/01/08 18:40:43 by mruggier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,58 @@ int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
+	int file[2];
+	int fd[2];
+
+	file[0] = open("file1", O_RDONLY);
+	file[1] = open("file2", O_WRONLY);
+	if (file[0] < 0 || file[1] < 0)
+	{
+		perror("open");
+		return (1);
+	}
+	if (dup2(file[1], STDOUT_FILENO) < 0)
+	{
+		perror("dup2");
+		return (1);
+	}
+
+	if (pipe(fd) < 0)
+	{
+		perror("pipe");
+		return (1);
+	}
 	
-	i have to write in the terminal ./pipex "ls -l" and put it in execve. but that function doesn't need "ls -l" but "ls" "-l", how ca i do?
-	char *args[] = {"ls", "-l", NULL};
-    execve("/bin/ls", args, NULL);
+	//check fork
+	if (fork() == 0) {
+        // primo comando: grep -o 'v' file1
+        dup2(file[0], STDIN_FILENO); // collega l'input della pipe allo stdin
+        dup2(fd[1], STDOUT_FILENO); // collega l'output della pipe allo stdout
+        close(fd[0]);
+        char *args[] = {"grep", "-o", "v", NULL};
+        execve("/bin/grep", args, NULL);
+    }
+
+    if (fork() == 0) {
+        // secondo comando: wc -l > file2
+        dup2(fd[0], STDIN_FILENO); // collega l'input della pipe allo stdin
+        dup2(file[1], STDOUT_FILENO); // collega l'output della pipe allo stdout
+        close(fd[1]);
+        char *args[] = {"wc", "-l", NULL};
+        execve("/usr/bin/wc", args, NULL);
+    }
+	
+	close(file[0]);
+    close(file[1]);
+	close(fd[0]);
+	close(fd[1]);
+    wait(NULL); // aspetta che il primo processo figlio termini
+    wait(NULL); // aspetta che il secondo processo figlio termini
+
+	
+	//fork per fare sia execve che stampare in 2 file
+	// char *args[] = {"ls", "-l", NULL};
+    // execve("/bin/ls", args, NULL);
+
+	//dup2 per scrivere quello che verrebbe in stdout in un file
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: mruggier <mruggier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 16:01:14 by mruggier          #+#    #+#             */
-/*   Updated: 2024/01/05 17:12:55 by mruggier         ###   ########.fr       */
+/*   Updated: 2024/01/08 18:12:35 by mruggier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@
 # include "libft/libft.h"
 # include "libft/ft_printf.h"
 # include "libft/get_next_line_bonus.h"
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 #endif
